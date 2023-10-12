0a3298d71563175b13d2cadbbc2b4362d922a303
Rajat Sharma <82762243+silver-times@users.noreply.github.com>
2023-10-12 22:37:00 +0200

    Add docker

---
+++ backend/config/database.ts
@@ -0,0 +1,9 @@
+import pg from 'pg';
+import {KEYS} from './keys';
+
+const {Client} = pg
+
+export const client = new Client({
+    connectionString: `postgres://${KEYS.DB_USER}:${KEYS.DB_PASSWORD}@${KEYS.DB_HOST}:${KEYS.DB_PORT}/${KEYS.DB_NAME}`
+});
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import {KEYS} from '../config/keys';
+import {client} from '../config/database';
 
 const app = express();
 
@@ -11,6 +12,13 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
 });
 
-app.listen(KEYS.PORT, () => {
-    console.log(`Server running on port ${KEYS.PORT}`);
+app.listen(KEYS.PORT, async () => {
+    try {
+        await client.connect();
+        console.log(`Server running on port ${KEYS.PORT}`);
+        console.log(`Database connected`)
+    }
+    catch (error) {
+        console.log(error);
+    }
 });
