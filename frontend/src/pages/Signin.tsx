2fcb0f35c0227ea30e608004bbaec52b8cd0c081
Rajat Sharma <82762243+silver-times@users.noreply.github.com>
2024-02-02 21:35:59 +0100

    fix: signin page

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useSignin } from "../hooks/useSignin";
-import SignInImage from "../assets/images/signin.jpg";
 
 const Signin = () => {
   const [email, setEmail] = useState("");
@@ -13,68 +12,63 @@ const Signin = () => {
 
   return (
     <div className="py-16">
-      <div className="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
-        <div
-          className="hidden lg:block lg:w-1/2 bg-center bg-no-repeat bg-cover"
-          style={{ backgroundImage: `url(${SignInImage})` }}
-        ></div>
-        <div className="w-full p-8 lg:w-1/2">
-          <h2 className="text-2xl font-semibold text-gray-700 text-center">
-            💪🏻workouts
-          </h2>
-          <p className="text-lg text-gray-600 text-center">Welcome back!</p>
+      <div className="bg-opacity-25 bg-blur-sm bg-black backdrop-filter backdrop-blur-sm  border border-white text-white shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl w-full p-8 lg:w-1/2">
+        <h2 className="text-4xl md:text-8xl font-semibold text-gray-700 text-center">
+          TRACKWISE
+        </h2>
+        <p className="text-2xl text-gray-600 text-center">Welcome back!</p>
 
-          <div className="mt-4 flex items-center justify-between">
-            <span className="border-b w-1/5 lg:w-1/4"></span>
-            <p className="text-xs text-center text-gray-500 uppercase">
-              login with email
-            </p>
-            <span className="border-b w-1/5 lg:w-1/4"></span>
-          </div>
-          <div className="mt-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Email Address
+        <div className="mt-4 flex items-center justify-between">
+          <span className="border-b w-1/5 lg:w-1/4"></span>
+          <p className="text-sm text-center text-gray-500 uppercase">
+            login with email
+          </p>
+          <span className="border-b w-1/5 lg:w-1/4"></span>
+        </div>
+        <div className="mt-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Email Address
+          </label>
+          <input
+            type="email"
+            value={email}
+            placeholder="Email"
+            onChange={(event) => setEmail(event.target.value)}
+            className="bg-gray-200 text-black text-xl focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+          />
+        </div>
+        <div className="mt-4">
+          <div className="flex justify-between">
+            <label className="block text-white text-md font-bold mb-2">
+              Password
             </label>
-            <input
-              type="email"
-              value={email}
-              placeholder="Email"
-              onChange={(event) => setEmail(event.target.value)}
-              className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-            />
           </div>
-          <div className="mt-4">
-            <div className="flex justify-between">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Password
-              </label>
-            </div>
-            <input
-              type="password"
-              value={password}
-              placeholder="Password"
-              onChange={(event) => setPassword(event.target.value)}
-              className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-            />
-          </div>
-          <div className="mt-8">
-            <button
-              onClick={handleSubmit}
-              className="my-4 block w-full px-4 py-4 bg-primary hover:bg-heading border-2 hover:text-primary rounded-lg text-2xl text-white "
-            >
-              Login
-            </button>
-            {error && <p className="text-warning">{error}</p>}
-          </div>
-          <div className="mt-4 flex items-center justify-between">
-            <span className="border-b w-1/5 md:w-1/4"></span>
-            <a href="/signup" className="text-xs text-gray-500 uppercase">
-              or sign up
-            </a>
-            <span className="border-b w-1/5 md:w-1/4"></span>
-          </div>
+          <input
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+            className="bg-gray-200 text-black text-xl focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
+          />
+        </div>
+        <div className="mt-8">
+          <button
+            onClick={handleSubmit}
+            className="my-4 block w-full px-4 py-4 bg-primary hover:bg-heading border-2 hover:text-primary rounded-lg text-2xl text-white "
+          >
+            Login
+          </button>
+          {error && <p className="text-warning">{error}</p>}
+        </div>
+        <div className="mt-4 flex items-center justify-between">
+          <span className="border-b w-1/5 md:w-1/4"></span>
+          <a href="/signup" className="text-xs text-gray-500 uppercase">
+            or sign up
+          </a>
+          <span className="border-b w-1/5 md:w-1/4"></span>
         </div>
       </div>
+      {/* </div> */}
     </div>
   );
 };
