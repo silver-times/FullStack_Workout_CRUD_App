48838f0e11d3411da280d26e28faaa3ee54d3f9d
Rajat Sharma <82762243+silver-times@users.noreply.github.com>
2024-02-03 12:15:00 +0100

    add: AWS Elastic Beanstalk

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,7 +1,8 @@
-const API_URL = "http://localhost:8800/api";
+const API_URL =
+  "http://workoutsapp-env.eba-uyvm8pbc.eu-central-1.elasticbeanstalk.com";
 
 export default {
-  signup: `${API_URL}/users/signup`,
-  signin: `${API_URL}/users/signin`,
-  workouts: `${API_URL}/workouts`,
+  signup: `${API_URL}/api/users/signup`,
+  signin: `${API_URL}/api/users/signin`,
+  workouts: `${API_URL}/api/workouts`,
 };
