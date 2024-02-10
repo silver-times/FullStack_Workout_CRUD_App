44a9225c3fe1b51c6cf05f3c13f9f6f5c0d74916
Rajat Sharma <82762243+silver-times@users.noreply.github.com>
2024-02-10 12:39:30 +0100

    fix: AWS EC2deployment

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,5 +1,4 @@
-const API_URL =
-  "https://workoutsapp-env.eba-uyvm8pbc.eu-central-1.elasticbeanstalk.com";
+const API_URL = "http://localhost:8800";
 
 export default {
   signup: `${API_URL}/api/users/signup`,
